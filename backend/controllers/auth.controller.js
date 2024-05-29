f6dea57f9f4097951204e2f4f31e198372ce6cf0
abir_al_mahmud <104026095+abiralmahmud42@users.noreply.github.com>
2024-05-29 23:50:36 +0600

    5th commit

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+// controllers/auth.controller.js
+export const signup = (req, res) => {
+    // Signup logic here
+    res.send("Signup successful!");
+};
+
+export const login = (req, res) => {
+    // Login logic here
+    res.send("Login successful!");
+};
+
+export const logout = (req, res) => {
+    // Logout logic here
+    res.send("Logout successful!");
+};
---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,11 @@
+// routes/auth.routes.js
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
+
+export default router;
